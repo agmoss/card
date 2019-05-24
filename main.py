e4ba398c1394368cc167123889d8556994ddd0c1
Andrew Moss <agordonmoss@gmail.com>
2019-05-23 19:14:34 -0600

    Formatting

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     ax.spines["left"].set_visible(True)  
 
     # Text
-    ax.text(45,0.7, 'Andrew Moss',fontsize=12,style = 'oblique',horizontalalignment='right')
+    ax.text(45,0.7, 'Andrew Moss',fontsize=14,style = 'oblique',horizontalalignment='right',weight = 'bold')
     ax.text(45,0.45, 'W: m0ss.dev',fontsize=8,horizontalalignment='right')
-    ax.text(45,0.35, 'E: andrew@m0ss.dev',fontsize=8,horizontalalignment='right')
-    ax.text(45,0.2, 'P: 403 690 2015',fontsize=8,horizontalalignment='right')
+    ax.text(45,0.32, 'E: andrew@m0ss.dev',fontsize=8,horizontalalignment='right')
+    ax.text(45,0.16, 'P: 403 690 2015',fontsize=8,horizontalalignment='right')
 
     # Plot Chi square survival function
     color=iter(cm.winter(np.linspace(0,1,10)))
@@ -28,5 +28,6 @@ if __name__ == "__main__":
         c=next(color) 
         ax.plot(x,scipy.special.chdtrc(i,x),c=c)
 
-    fig.tight_layout()
+    fig.tight_layout(pad=0.5)
+    plt.savefig("card.png",bbox_inches='tight', pad_inches=0,dpi=900)
     plt.show()
