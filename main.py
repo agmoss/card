ba0715541eadf0dff501eea8400f788f7f246b6c
Andrew Moss <andrew@m0ss.dev>
2020-02-02 12:37:17 -0700

    linting and formatting

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-import scipy.special
-import matplotlib.pyplot as plt
-import pylab
+"""Card"""
 import numpy as np
+import scipy.special
 import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-    fig, ax = plt.subplots()
-    fig.set_size_inches(3.5, 2, forward=True)
+    FIG, AX = plt.subplots()
+    FIG.set_size_inches(3.5, 2, forward=True)
 
     # Remove the plot frame lines
-    ax.spines["top"].set_visible(False)    
-    ax.spines["bottom"].set_visible(True)    
-    ax.spines["right"].set_visible(False)    
-    ax.spines["left"].set_visible(True)  
+    AX.spines["top"].set_visible(False)
+    AX.spines["bottom"].set_visible(True)
+    AX.spines["right"].set_visible(False)
+    AX.spines["left"].set_visible(True)
 
     # Text
-    ax.text(45,0.7, 'Andrew Moss',fontsize=14,style = 'oblique',horizontalalignment='right',weight = 'bold')
-    ax.text(45,0.45, 'W: m0ss.dev',fontsize=8,horizontalalignment='right')
-    ax.text(45,0.32, 'E: andrew@m0ss.dev',fontsize=8,horizontalalignment='right')
-    ax.text(45,0.16, 'P: 403 690 2015',fontsize=8,horizontalalignment='right')
+    AX.text(
+        45,
+        0.7,
+        "Andrew Moss",
+        fontsize=14,
+        style="oblique",
+        horizontalalignment="right",
+        weight="bold",
+    )
+    AX.text(45, 0.45, "W: m0ss.dev", fontsize=8, horizontalalignment="right")
+    AX.text(45, 0.32, "E: andrew@m0ss.dev", fontsize=8, horizontalalignment="right")
+    AX.text(45, 0.16, "P: 403 690 2015", fontsize=8, horizontalalignment="right")
 
     # Plot Chi square survival function
-    color=iter(cm.winter(np.linspace(0,1,10)))
-    x = np.linspace(0,50,500)
+    CMAP = cm.get_cmap("winter")
+    COLOR = iter(CMAP(np.linspace(0, 1, 10)))
+    X = np.linspace(0, 50, 500)
     for i in range(10):
-        c=next(color) 
-        ax.plot(x,scipy.special.chdtrc(i,x),c=c)
+        c = next(COLOR)
+        AX.plot(X, scipy.special.chdtrc(i, X), c=c)
 
-    fig.tight_layout(pad=0.5)
-    plt.savefig("card.png",bbox_inches='tight', pad_inches=0,dpi=900)
+    FIG.tight_layout(pad=0.5)
+    plt.savefig("./dist/card.png", bbox_inches="tight", pad_inches=0, dpi=900)
     plt.show()
